d4004068651c21704612e8128b754ba1e1ed4658
jackcaserta <jcaserta@albany.edu>
2016-11-20 19:06:55 -0500

    Add files via upload

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,37 +1,54 @@
-//CompSci210
-//H5.0 Sorting
 //Jack Caserta
-import java.util.Random;
-import java.util.Arrays;
-
-      class BubbleSort{
-        public static void main(String [] args){
-    Random g = new Random();
-
-    int [] number = new int [10];
-
-    System.out.print("Random Numbers:");
-    for (int d = 0 ; d<number.length ; d++){
-        int RandomG = g.nextInt(100)+1;
-        System.out.print("\t" + RandomG);
+//CSI 213
+//Magnus
+//H5.1 Sorting
+//BubbleSortProgram
+ 
+public class BubbleSort {
+ 
+        public static void main(String[] args) {
+               
+                //create the array that will be sorted
+                int intArray[] = new int[]{5,90,35,45,150,3};
+               
+                //the array will print prior to being sorted
+                System.out.println("Array Prior To Bubble Sort");
+                for(int i=0; i < intArray.length; i++){
+                        System.out.print(intArray[i] + " ");
+                }
+               
+                //the array is sorted after bublle sort algorithm
+                bubbleSort(intArray);
+               
+                System.out.println("");
+               
+                //print the array post bubble sort algorithm
+                System.out.println("Array Post Bubble Sort");
+                for(int i=0; i < intArray.length; i++){
+                        System.out.print(intArray[i] + " ");
+                }
+ 
         }
-
-  System.out.print("\nSorted Numbers:"+Arrays.toString(BubbleSortAsceMethod(number)));
-
-                     }
-     public static int [] BubbleSortAsceMethod(int[] x){
-    int temp;
-
-    for(int i = 0 ; i < x.length-1 ; i++){
-        for ( int j = 1 ; j < x.length-1 ; j++){
-            if ( x[j-1] < x[j]){
-                temp = x[j-1];
-                x[j-1] = x[j];
-                x[j] = temp;
-            }
-
-            }
+ 
+        private static void bubbleSort(int[] intArray) {
+               
+               
+                int n = intArray.length;
+                int temp = 0;
+               
+                for(int i=0; i < n; i++){
+                        for(int j=1; j < (n-i); j++){
+                               
+                                if(intArray[j-1] > intArray[j]){
+                                        
+                                        temp = intArray[j-1];
+                                        intArray[j-1] = intArray[j];
+                                        intArray[j] = temp;
+                                }
+                               
+                        }
                 }
-        return x;   
-                }
-                          }
+       
+        }
+}
+ 
